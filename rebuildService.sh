fe305c7e7cf2d16432f5d63c6e218b05705d61f4
Pat Haggerty <hagg@haggconsulting.com>
2020-02-22 13:21:05 -0600

    Update rebuildService.sh

--- rebuildService.sh
+++ rebuildService.sh
@@ -1,3 +1,2 @@
-export PROJECT=$(gcloud config list --format 'value(core.project)')
-gcloud builds submit --tag gcr.io/$PROJECT/hello-logging
-gcloud run deploy hello-logging --image gcr.io/$PROJECT/hello-logging --region us-central1 --platform managed --quiet --allow-unauthenticated --concurrency 5
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/hello-logging
+gcloud run deploy hello-logging --image gcr.io/$GOOGLE_CLOUD_PROJECT/hello-logging --region us-central1 --platform managed --quiet --allow-unauthenticated --concurrency 5 --labels stage=dev,department=training
