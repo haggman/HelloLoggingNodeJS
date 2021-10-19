49a2ecfdaf6c48ab3d933e4c9f864d1e54918095
haggman <40147584+haggman@users.noreply.github.com>
2021-10-19 10:37:15 -0500

    Cap instance count at 3, up concurrency

--- rebuildService.sh
+++ rebuildService.sh
@@ -1,2 +1,2 @@
 gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/hello-logging:1.0
-gcloud run deploy hello-logging --image gcr.io/$GOOGLE_CLOUD_PROJECT/hello-logging:1.0 --region us-central1 --platform managed --quiet --allow-unauthenticated --concurrency 5 --labels stage=dev,department=training
+gcloud run deploy hello-logging --image gcr.io/$GOOGLE_CLOUD_PROJECT/hello-logging:1.0 --region us-central1 --quiet --allow-unauthenticated --concurrency 80 --max-instances 3 --labels stage=dev,department=training
