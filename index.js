5fbb93a416c71e462df9459bf2d4f3c55ce122ab
Patrick Haggerty <hagg@haggconsulting.com>
2023-06-21 16:36:31 -0500

    Clean up comments and switch to batch processor

--- index.js
+++ index.js
@@ -10,15 +10,12 @@
 //Using the newer OpenTelemetry API
 const opentelemetry = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { SimpleSpanProcessor, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const {
   TraceExporter,
 } = require("@google-cloud/opentelemetry-cloud-trace-exporter");
 
 // Enable OpenTelemetry exporters to export traces to Google Cloud Trace.
-// Exporters use Application Default Credentials (ADCs) to authenticate.
-// See https://developers.google.com/identity/protocols/application-default-credentials
-// for more details.
 const provider = new NodeTracerProvider();
 
 // Initialize the exporter. When your application is running on Google Cloud,
@@ -26,7 +23,7 @@ const provider = new NodeTracerProvider();
 const exporter = new TraceExporter();
 
 // Configure the span processor to send spans to the exporter
-provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
 // ******
 // Enable Error Reporting
